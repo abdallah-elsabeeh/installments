4f1157cb2a51f0e47d7018114db7c43ebce7d408
abdallah elsabeeh <abdallahs@motory.com>
2018-09-28 18:58:13 +0300

    setup 'gridview' module in Yii configuration file. 	modified:   config/web.php

--- config/web.php
+++ config/web.php
@@ -39,14 +39,17 @@ $config = [
             ],
         ],
         'db' => $db,
-        
 //        'urlManager' => [
 //            'enablePrettyUrl' => true,
 //            'showScriptName' => false,
 //            'rules' => [
 //            ],
 //        ],
-        
+    ], 'modules' => [
+
+        'gridview' => [
+            'class' => '\kartik\grid\Module'
+        ]
     ],
     'params' => $params,
 ];
@@ -56,15 +59,15 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+            // uncomment the following to add your IP if you are not connecting from localhost.
+            //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['*'],
     ];
 }
 
